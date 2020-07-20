8a8424f4c8509f95e83529de72d6e64af91799d9
João Martins <45777813+xpto051@users.noreply.github.com>
2020-07-20 02:04:13 +0100

    12.2

--- Server/models/generate-class.js
+++ Server/models/generate-class.js
@@ -1,50 +1,83 @@
-var mustache = require('mustache');
-var fs = require('fs');
-var { resolve } = require('path');
+var mustache = require("mustache");
+var fs = require("fs");
+var { resolve } = require("path");
 
-let rawdata = fs.readFileSync('./Server/config.json');
+let rawdata = fs.readFileSync("./Server/config.json");
 let config = JSON.parse(rawdata);
 
 function classGenerator() {
-    config.schemas.forEach(schema => {
-        var path = resolve(__dirname, '..') + schema.path;
-        fs.readFile(path, function (err, data) {
-            let construtor = [];
-            let enumerables = [];
-            let references = [];
-            var string = JSON.parse(data.toString());
+  config.schemas.forEach((schema) => {
+    var path = resolve(__dirname, "..") + schema.path;
+    fs.readFile(path, function (err, data) {
+      let construtor = [];
+      let enumerables = [];
+      let references = [];
+      let manyfunc = [];
+      var string = JSON.parse(data.toString());
 
-            Object.keys(string.properties).forEach(function (key, id, array) {
-                (id === array.length - 1) ? construtor.push({ name: key, comma: false }) : construtor.push({ name: key, comma: true });
+      Object.keys(string.properties).forEach(function (key, id, array) {
+        id === array.length - 1
+          ? construtor.push({ name: key, comma: false })
+          : construtor.push({ name: key, comma: true });
 
-                if (string.required && !string.required.includes(key)) {
-                    enumerables.push(({ name: key }));
-                }
+        if (string.required && !string.required.includes(key)) {
+          enumerables.push({ name: key });
+        }
+      });
+
+      if (string.references != null) {
+        Object.keys(string.references).forEach((ref) => {
+          atrb = string.references[ref];
+          if (atrb.relation == "M-M") {
+            manyfunc.push({
+              classTitle: string.title,
+              ReftableName: atrb.model,
+              classTitleLower: string.title.toLowerCase(),
             });
-
-            if (string.references != null) {
-                Object.keys(string.references).forEach(ref => {
-                    atrb = string.references[ref];
-                    references.push({ ReftableName: atrb.model.toLowerCase() });
-                });
-            }
-
-            var view = {
-                classTitle: string.title,
-                classTitleLower: string.title.toLowerCase(),
-                constructorArguments: Object.keys(string.properties).join(),
-                classConstructor: construtor,
-                classEnumerables: enumerables,
-                referencesEnumerables: references,
-                dbname: config.dbname
-            }
-
-            fs.readFile('Server/models/class.mustache', function (err, data1) {
-                var output = mustache.render(data1.toString(), view);
-                fs.writeFileSync('Publish/Models/' + string.title + '.js', output);
-            });
+          } else {
+            references.push({ ReftableName: atrb.model.toLowerCase() });
+          }
         });
+      }
+      //percorrer os outros shemas todos para verificar se é encessario incluir many func
+      config.schemas.forEach((shemaWithMM) => {
+        var path = resolve(__dirname, "..") + shemaWithMM.path;
+        fs.readFile(path, function (err, data) {
+          var stringMM = JSON.parse(data.toString());
+
+          if (stringMM.references != null) {
+            Object.keys(stringMM.references).forEach((refMM) => {
+              atrbMM = stringMM.references[refMM];
+              if (atrbMM.model == string.title) {
+                //gerar funçao many como tabelada alternativa
+                manyfunc.push({
+                  classTitle: string.title,
+                  ReftableName: atrb.model,
+                  classTitleLower: string.title.toLowerCase(),
+                });
+              }
+            });
+          }
+        });
+      });
+
+      var view = {
+        classTitle: string.title,
+        classTitleLower: string.title.toLowerCase(),
+        constructorArguments: Object.keys(string.properties).join(),
+        classConstructor: construtor,
+        classEnumerables: enumerables,
+        referencesEnumerables: references,
+        isManyToMany: manyfunc,
+        dbname: config.dbname,
+      };
+
+      fs.readFile("Server/models/class.mustache", function (err, data1) {
+        var output = mustache.render(data1.toString(), view);
+        fs.writeFileSync("Publish/Models/" + string.title + ".js", output);
+      });
     });
+  });
 }
 
 module.exports.classGenerator = classGenerator;
